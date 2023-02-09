cbc21d8208d19dd91c9af5d2af62c392c8d26a6a
yooooSW <pk8652@naver.com>
2023-02-09 16:30:38 +0900

    nameclass 생성

--- 01_basic/08_file_ex.py
+++ 01_basic/08_file_ex.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # f = open('01_basic/test.txt','r',encoding='cp949')
 # # print(type(f))
 # text = f.read()
@@ -36,43 +35,3 @@ import json
 with open('01_basic/jsontest.json', 'r') as f:
     data = json.load(f)
     print(type(data))
-=======
-# f = open('01_basic/test.txt','r',encoding='cp949')
-# # print(type(f))
-# text = f.read()
-# print(text)
-# f.close()
-
-# with open('01_basic/test.txt','r',encoding='cp949') as f:
-#     while True:
-#         text = f.readline()
-#         if not text:
-#             break
-#         print(text)
-
-# with open('01_basic/test_w.txt','w',encoding='utf8') as f:
-#     f.write('첫줄 입력\n')
-
-import pickle
-
-card=[{'name':'홍길동','address':'광주','tel':'111-2222-3333','email':'hong@naver.com'}, 
-       {'name':'김놀부','address':'부산','tel':'111-2222-3333','email':'kim@naver.com'}]
-
-# f = open('01_basic/pickledata.pickle','wb')
-# pickle.dump(card,f)
-# f.close()
-
-# with open('01_basic/pickledata.pickle','rb') as f:
-#     data = pickle.load(f)
-#     print(data)
-
-import json
-
-# with open('01_basic/jsontest.json', 'w') as f:
-#     json.dump(card,f,indent=2)
-
-with open('01_basic/jsontest.json', 'r') as f:
-    data = json.load(f)
-    print(type(data))
->>>>>>> origin/master
-    print(data)
---
+++ 01_basic/17_namecard_class.py
@@ -0,0 +1,69 @@
+# 클래스 Card,  CardBook
+# --------------------------------------------------------------------------------
+# 1.CardBook 생성 2.Card 추가 3.Card 삭제 4.Card 내용 보기 5.전체목록 보기 6.종료
+# --------------------------------------------------------------------------------
+# >>> ''')
+
+import namecard as nc
+import pickle
+
+cardbook = None
+
+with open('01_basic/cardbook.pickle', 'rb') as f:
+    cardbook = pickle.load(f)
+
+while True:
+    menu = input('''
+--------------------------------------------------------------------------------
+1.CardBook 생성 2.Card 추가 3.Card 삭제 4.Card 내용 보기 5.전체목록 보기 6.종료
+--------------------------------------------------------------------------------
+>>> ''')
+    if menu == '1':
+        if cardbook == None:
+            cardbook = nc.CardBook(input('타이틀을 입력하세요.'))
+        else:
+            print('생성된 cardbook이 존재합니다.')
+    elif menu == '2':
+        if cardbook == None:
+            print('cardbook 생성한 후 추가 가능합니다.')
+        else:
+            name = input('이름을 입력하세요 >>> ')     
+            address = input('주소를 입력하세요 >>> ')
+            tel = input('전화번호를 입력하세요 >>> ')
+            email = input('이메일을 입력하세요 >>> ')
+            card = nc.Card(name,address,tel,email)
+            cardbook.add_card(card)
+    elif menu == '3':
+        if cardbook == None:
+            print('cardbook 생성한 후 추가 가능합니다.')
+        else:
+            print(list(cardbook.cards.keys()))
+            page = int(input('page number >>> '))
+            card = cardbook.remove_card(page)
+            print(card, '-> 삭제')
+    elif menu == '4':
+        if cardbook == None:
+            print('cardbook 생성한 후 추가 가능합니다.')
+        else:
+            print(list(cardbook.cards.keys()))
+            page = int(input('page number >>> '))
+            card = cardbook.cards[page]
+            print(card)      
+    elif menu == '5':
+        if cardbook == None:
+            print('cardbook 생성한 후 가능합니다.')
+        else:
+            key = input('정렬 키(입력값:name,address,tel,email) >>> ')
+            sort = bool(input('오름차순(enter),내림차순(1) >>>'))
+            # print(sort,type(sort))
+            if key in ('name','address','tel','email'):
+                cardbook.list_cards(key,sort)
+            else:
+                cardbook.list_cards(reverse=sort)
+    elif menu == '6':
+        print('프로그램 종료')
+        break
+
+
+with open('01_basic/cardbook.pickle','wb') as f:
+    pickle.dump(cardbook,f)
